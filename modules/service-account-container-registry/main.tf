91f030412a9bf31faf3a696e1b653cdd5bd82182
David Mikalova <324548+dmikalova@users.noreply.github.com>
2021-10-15 20:41:39 -0700

    Removed unused data

--- modules/service-account-container-registry/main.tf
+++ modules/service-account-container-registry/main.tf
@@ -1,14 +1,10 @@
-locals {
-  secret_name = "docker-registry-${var.container_registry_info.name}-credentials"
-}
-
 resource "digitalocean_container_registry_docker_credentials" "main" {
   registry_name = var.container_registry_info.name
 }
 
 resource "kubernetes_secret" "main" {
   metadata {
-    name = local.secret_name
+    name = "docker-registry-${var.container_registry_info.name}-credentials"
   }
 
   data = {
@@ -21,7 +17,7 @@ resource "kubernetes_secret" "main" {
 resource "kubernetes_default_service_account" "main" {
   automount_service_account_token = false
   image_pull_secret {
-    name = local.secret_name
+    name = kubernetes_secret.main.metadata[0].name
   }
   metadata {
     namespace = var.service_account_conf.namespace
