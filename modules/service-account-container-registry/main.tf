38b752ad3b6ac84029f822475b2e65ba0474eadf
David Mikalova <324548+dmikalova@users.noreply.github.com>
2021-10-22 19:05:25 -0700

    Fixed default secret

--- modules/service-account-container-registry/main.tf
+++ modules/service-account-container-registry/main.tf
@@ -2,6 +2,16 @@ resource "digitalocean_container_registry_docker_credentials" "main" {
   registry_name = var.container_registry_info.name
 }
 
+resource "kubernetes_default_service_account" "main" {
+  automount_service_account_token = true
+  image_pull_secret {
+    name = kubernetes_secret.main.metadata[0].name
+  }
+  metadata {
+    namespace = var.service_account_conf.namespace
+  }
+}
+
 resource "kubernetes_secret" "main" {
   metadata {
     name = "docker-registry-${var.container_registry_info.name}-credentials"
@@ -12,14 +22,4 @@ resource "kubernetes_secret" "main" {
   }
 
   type = "kubernetes.io/dockerconfigjson"
-}
-
-resource "kubernetes_default_service_account" "main" {
-  automount_service_account_token = false
-  image_pull_secret {
-    name = kubernetes_secret.main.metadata[0].name
-  }
-  metadata {
-    namespace = var.service_account_conf.namespace
-  }
 }
