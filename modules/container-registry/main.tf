cc38236cbfb53b6fbf98bdd566ebd4568fc51e22
David Mikalova <324548+dmikalova@users.noreply.github.com>
2021-11-05 23:28:16 -0700

    Updated outputs

--- modules/container-registry/main.tf
+++ modules/container-registry/main.tf
@@ -2,3 +2,13 @@ resource "digitalocean_container_registry" "main" {
   name                   = var.container_registry_conf.name
   subscription_tier_slug = var.container_registry_conf.subscription_tier_slug
 }
+
+resource "digitalocean_container_registry_docker_credentials" "read" {
+  registry_name = digitalocean_container_registry.main.name
+  write         = false
+}
+
+resource "digitalocean_container_registry_docker_credentials" "write" {
+  registry_name = digitalocean_container_registry.main.name
+  write         = true
+}
--- modules/container-registry/outputs.tf
+++ modules/container-registry/outputs.tf
@@ -1,5 +1,10 @@
 output "info" {
+  sensitive = true
   value = {
-    name = digitalocean_container_registry.main.name
+    credentials_read  = digitalocean_container_registry_docker_credentials.read.docker_credentials
+    credentials_write = digitalocean_container_registry_docker_credentials.write.docker_credentials
+    endpoint          = digitalocean_container_registry.main.endpoint
+    registry_name     = digitalocean_container_registry.main.name
+    server_url        = digitalocean_container_registry.main.server_url
   }
 }
